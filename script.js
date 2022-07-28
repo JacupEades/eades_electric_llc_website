86d831f623e2b5ef89ad08740f76e95a90c23a2e
Jacob Eades <jacob.eades@eadeselectric.com>
2022-07-28 19:19:43 -0400

    Major content complete

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 "use strict";
+////////////////W3 example///////////////////////
+// function burgerOpenClose() {
+//   var x = document.getElementById("nav-menu-link");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
 
-function burgerOpenClose() {
-  var x = document.getElementById("nav-menu-link");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+////////////////OmniFood example///////////////////////
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
